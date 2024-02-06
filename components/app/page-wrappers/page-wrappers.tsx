099e9b8e2491cc971f04460ad56f7fed53d20358
mufaroxyz <mufaro.priv@gmail.com>
2024-02-06 22:54:25 +0100

    style: linting

--- components/app/page-wrappers/page-wrappers.tsx
+++ components/app/page-wrappers/page-wrappers.tsx
@@ -1,26 +1,27 @@
-
 interface PageHeaderProps {
-    title? : string;
-    children: React.ReactNode;
+  title?: string;
+  children: React.ReactNode;
 }
 
 const PageHeader = ({ title, children }: PageHeaderProps) => {
-    return (
-        <>
-            {title ? <header className="flex flex-col gap-2">
-                <h1 className="text-xl font-bold text-white">{title}</h1>
-                {children}
-            </header> : null}
-        </>
-    )
-}
+  return (
+    <>
+      {title ? (
+        <header className="flex flex-col gap-2">
+          <h1 className="text-xl font-bold text-white">{title}</h1>
+          {children}
+        </header>
+      ) : null}
+    </>
+  );
+};
 
-interface PageLineSeperator {};
+interface PageLineSeperator {}
 
 const PageLineSeperator = ({}: PageLineSeperator) => {
-    return (
-        <div className="w-[calc(100%-0.5rem)] h-[2px] my-2 rounded-xl bg-accent-border" />
-    )
-}
+  return (
+    <div className="w-[calc(100%-0.5rem)] h-[4px] my-2 rounded-xl bg-input" />
+  );
+};
 
-export { PageHeader, PageLineSeperator }
+export { PageHeader, PageLineSeperator };
